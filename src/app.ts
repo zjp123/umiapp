b2980120c6997720955d3c900dfccbe080cab1c2
zhangjunpeng <zhangjunpeng@lexue.com>
2022-05-04 20:36:15 +0800

    全部跑通-主子应用通信-这是子项目1，umi项目

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ export const qiankun = {
   // 应用 render 之前触发
   async mount(props: any) {
     console.log('app1 mount', props);
+    props.onGlobalStateChange((state: any, prev: any) => {
+      // state: 变更后的状态; prev 变更前的状态
+      console.log(state, prev, '哈哈哈哈');
+    });
   },
   // 应用卸载之后触发
   async unmount(props: any) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import styles from './index.less';
 import 'antd/dist/antd.css';
+import { useEffect } from 'react';
+import { connectMaster } from 'umi';
+// import { useModel } from 'umi';
 
-export default function IndexPage() {
+function IndexPage(props: any) {
+  // const masterProps = useModel('@@qiankunStateFromMaster');
+  useEffect(() => {
+    setTimeout(() => {
+      props.setMasterState({ str: 'bbb' });
+    }, 2000);
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>子应用app1{props.masterState.str}</h1>
+      {/* <h1 className={styles.title}>子应用app1{masterProps.masterState.str}</h1> */}
     </div>
   );
 }
+
+export default connectMaster(IndexPage);
